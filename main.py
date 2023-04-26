bf02e0c56baf8b2d9f8524e884697c99fc09cd49
Lazaros Topalis <lazarostop32@gmail.com>
2023-04-26 23:12:27 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+data = pd.read_csv("https://www.stats.govt.nz/assets/Uploads/Effects-of-COVID-19-on-trade/Effects-of-COVID-19-on-trade"
+                "-At-15-December-2021-provisional/Download-data/effects-of-covid-19-on-trade-at-15-december-2021"
+                "-provisional.csv")
+
+print(type(data))
+print(data)
+
