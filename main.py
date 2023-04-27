47f4fe95200aaeb1b0a1f8bc5886d60d63ce030f
Lazaros Topalis <lazarostop32@gmail.com>
2023-04-27 19:36:12 +0300

    turnover by country

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 import pandas as pd
+from datetime import datetime
 
-data = pd.read_csv("https://www.stats.govt.nz/assets/Uploads/Effects-of-COVID-19-on-trade/Effects-of-COVID-19-on-trade"
-                "-At-15-December-2021-provisional/Download-data/effects-of-covid-19-on-trade-at-15-december-2021"
-                "-provisional.csv")
 
-print(type(data))
-print(data)
+def read_data_from_url(
+        url="https://www.stats.govt.nz/assets/Uploads/Effects-of-COVID-19-on-trade/Effects-of-COVID-19-on-trade-At-15"
+            "-December-2021-provisional/Download-data/effects-of-covid-19-on-trade-at-15-december-2021-provisional"
+            ".csv"):
+    return pd.read_csv(url)
 
+
+def turnover_by_country(dataset):
+    countries = list(set(dataset['Country']))
+
+    results = dict()
+    for country in countries:
+        data_in_tones = dataset.loc[dataset['Country'] == country].loc[dataset['Measure'] == 'Tonnes']
+        data_in_dollars = dataset.loc[dataset['Country'] == country].loc[dataset['Measure'] == '$']
+
+        sum_in_tones = data_in_tones['Value'].sum()
+        sum_in_dollars = data_in_dollars['Value'].sum()
+
+        value = (sum_in_tones, sum_in_dollars)
+        results[country] = value
+
+    return results
+
+
+data = read_data_from_url("effects-of-covid-19-on-trade-at-15-december-2021-provisional.csv")
+result = turnover_by_country(data)
+print(result)
