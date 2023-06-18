0abd5e8c8f0d02275addfc5df8976eeb6f9767ce
Lazaros Topalis <83336644+ltopalis@users.noreply.github.com>
2023-06-18 12:59:58 +0300

    windows virtual environment

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+# activate virtual environment (Windows)
+# cd windows_venv/Lib
+# activate
+#
+# activate virtual environment (Linux)
+# source linux_venv/bin/activate
+
 from GUIs import *
 from dataExtractor import DataExtractor
 
